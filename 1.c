0fe7429beccc65d4f2a6ead2f6208f52462819c5
Selvaraj12345 <selvacse37@gmail.com>
2017-06-20 21:44:56 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  
+#include <conio.h>   
+int main() {  
+    int num;  
+    printf("Enter a Number\n");  
+    scanf("%d", &num);  
+    if(num > 0) {  
+    printf("%d is Positive Number", num);  
+    } else if (num < 0) {  
+    printf("%d is Negative Number", num);  
+    } else {  
+    printf("Input Number is Zero");  
+    }  
+    return 0;  
+}
